26e079ef28065a4da094f1204a6c3dafdd8db420
BINCY MARIYAM <bincymariyam97@gmail.com>
2022-12-13 13:15:23 +0530

    Intial commit

--- App.js
+++ App.js
@@ -1,10 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { StatusBar } from "expo-status-bar";
+
+import { StyleSheet, Text, View, TextInput } from "react-native";
+
+const API_KEY = "5e126929af692a4c53feb3ad457f97ab";
 
 export default function App() {
+  const [text, setText] = useState("");
+
+  const [data, setData] = useState(null);
+
+  function getWeatherData(query) {
+    const BASE_URL = `https://api.openweathermap.org/data/2.5/weather?q=${query}&APPID=${API_KEY}`;
+
+    fetch(BASE_URL)
+      .then((response) => response.json())
+      .then((newdata) => setData(newdata.main))
+      .catch((err) => console.log(err));
+  }
+
+  useEffect(() => {
+    getWeatherData(text);
+  }, [text]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput
+        value={text}
+        onChangeText={setText}
+        style={styles.input}
+        placeholder="Enter City"
+      />
+
+      {data ? (
+        <>
+          <Text>Current temperature : {data.temp}</Text>
+          <Text>Humidity : {data.humidity}</Text>
+        </>
+      ) : (
+        <Text>No data</Text>
+      )}
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +49,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
 });
